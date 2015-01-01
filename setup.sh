33353ee7d57ec9a5dbdf98bfc017c9e7d12d97bc
Fahad Saleh <fahad1116@icloud.com>
2015-01-01 12:52:50 +0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-ln -s .vimrc ~/.vimrc
-ln -s .vim/ ~/.vim
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .profile ~/.profile
-ln -s .config/terminator ~/.config
-ln -s .bashrc ~/.bashrc
+cd ~/dotfiles/dotfiles  
+   
+ln -s ~/dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.vim .vim/
+ln -s ~/dotfiles/.tmux.conf .tmux.conf
+ln -s ~/dotfiles/.config/terminator .config/terminator
+ln -s ~/dotfiles/.bashrc .bashrc
+
