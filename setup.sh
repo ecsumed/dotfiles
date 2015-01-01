58c811fa90ac6f0ab0d6e9d0853e1f99d63c7571
Fahad Saleh <fahad1116@icloud.com>
2015-01-01 14:01:13 +0700

    removed 'deleted' files from git status

--- setup.sh
+++
@@ -1,8 +0,0 @@
-cd ~/dotfiles/dotfiles  
-   
-ln -s ~/dotfiles/.vimrc .vimrc
-ln -s ~/dotfiles/.vim .vim/
-ln -s ~/dotfiles/.tmux.conf .tmux.conf
-ln -s ~/dotfiles/.config/terminator .config/terminator
-ln -s ~/dotfiles/.bashrc .bashrc
-
